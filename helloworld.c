26ed380fdde08e124b1c7f2ff6308bd515ef8605
Seung20 <55528338+Seung20@users.noreply.github.com>
2023-04-12 22:30:13 +0900

    가지 3

--- helloworld.c
+++ helloworld.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("This is branch3\n");
-	printf("Good morning!\n");
-	printf("Hi\n");
-	printf("Hello World!\n");
-	printf("This is branch3\n");
+	printf("Thank you\n");
 	return 0;
 
 }
